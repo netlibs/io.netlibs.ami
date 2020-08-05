108c24ecf1a795f75c5d54770e5b64c44fdc1368
Theo Zourzouvillys <zourzouvillys@users.noreply.github.com>
2020-08-05 11:36:48 -0700

    Update AmiFrameDecoder.java

--- netty/src/main/java/io/netlibs/ami/netty/AmiFrameDecoder.java
+++ netty/src/main/java/io/netlibs/ami/netty/AmiFrameDecoder.java
@@ -1,9 +1,9 @@
 package io.netlibs.ami.netty;
 
-import static java.nio.charset.StandardCharsets.US_ASCII;
+import java.nio.charset.StandardCharsets;
 
 import com.google.common.base.Splitter;
-import com.google.common.base.Splitter.MapSplitter;
+import com.google.common.base.Strings;
 
 import io.netlibs.ami.api.AmiVersion;
 import io.netty.buffer.ByteBuf;
@@ -16,16 +16,46 @@ import io.netty.channel.SimpleChannelInboundHandler;
 
 public class AmiFrameDecoder extends SimpleChannelInboundHandler<ByteBuf> {
 
-  private MapSplitter splitter = Splitter.on("\r\n").withKeyValueSeparator(Splitter.onPattern("[\\t ]*:[\t ]*").limit(2).trimResults());
+  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AmiFrameDecoder.class);
+  private Splitter splitter = Splitter.on("\r\n");
 
   public AmiFrameDecoder(AmiVersion version) {
   }
 
   @Override
   protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+
+    // first, split on newlines.
+
     DefaultAmiFrame frame = DefaultAmiFrame.newFrame();
-    splitter.split(msg.toString(US_ASCII)).forEach(frame::add);
+
+    splitter.split(msg.toString(StandardCharsets.UTF_8))
+      .forEach(line -> {
+
+        if (Strings.isNullOrEmpty(line)) {
+          return;
+        }
+
+        int idx = line.indexOf(':');
+
+        if (idx == -1) {
+          log.error("frame line was invalid: [{}]", line);
+          return;
+        }
+
+        String name = line.substring(idx).trim();
+        String value = line.substring(idx + 1).trim();
+
+        if (frame.contains(name)) {
+          log.warn("duplicate key '{}', values: {}, adding '{}'", name, frame.getAll(name), value);
+        }
+
+        frame.add(name, value);
+
+      });
+
     ctx.fireChannelRead(frame);
+
   }
 
 }
